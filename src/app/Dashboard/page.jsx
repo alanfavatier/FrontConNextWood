d671a895bf6b2d363673d8081ecc4e311e655774
alanfavatier <alanfavatier305@gmail.com>
2024-07-29 20:02:39 -0300

    arreglos

--- src/app/Dashboard/page.jsx
+++ src/app/Dashboard/page.jsx
@@ -1,11 +1,16 @@
 "use client";
 import { useAppSelector } from "../../redux/hooks";
-import React from "react";
+import React, { useDispatch } from "react";
 import VendedorPage from "../../components/Dashboard/VendedorPage";
 import AdminPage from "../../components/Dashboard/AdminPage";
 
 const Page = () => {
   const user = useAppSelector((state) => state.useReducer.user);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getlogindata());
+  }, [dispatch]);
 
   // Decide qué componente renderizar basado en el rol del usuario
   const SelectedPage = user?.role === "Admin" ? AdminPage : VendedorPage;
