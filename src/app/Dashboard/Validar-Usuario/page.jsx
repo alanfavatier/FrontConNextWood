7bca981e235cfb00c27136b5dd80978744452082
alanfavatier <alanfavatier305@gmail.com>
2024-07-29 21:01:09 -0300

    users

--- src/app/Dashboard/Validar-Usuario/page.jsx
+++ src/app/Dashboard/Validar-Usuario/page.jsx
@@ -3,8 +3,21 @@
 import { data } from "../../../../public/data";
 import Image from "next/image";
 
+import { useState } from "react";
+import { toast, Toaster } from "react-hot-toast";
+
 const Page = () => {
-  const usuarios = data.users;
+  const [usuarios, setUsuarios] = useState(data.users);
+
+  const handleValidar = (id) => {
+    setUsuarios(usuarios.filter((user) => user._id !== id));
+    toast.success("Usuario Validado");
+  };
+
+  const handleInvalidar = (id) => {
+    setUsuarios(usuarios.filter((user) => user._id !== id));
+    toast.error("Usuario Invalidado");
+  };
 
   return (
     <div className="p-6 items-center">
@@ -19,8 +32,8 @@ const Page = () => {
                   <th className="text-center p-2">Nombre</th>
                   <th className="text-center p-2">Email</th>
                   <th className="text-center p-2">Rol</th>
-                  <th className="text-center p-2">Telefono</th>
-                  <th className="text-center p-2">Direccion</th>
+                  <th className="text-center p-2">Teléfono</th>
+                  <th className="text-center p-2">Dirección</th>
                 </tr>
               </thead>
               <tbody>
@@ -41,12 +54,18 @@ const Page = () => {
                     <td className="p-2 text-center">{item.phone}</td>
                     <td className="p-2 text-center">{item.address.city}</td>
                     <td className="p-2 text-center">
-                      <button className="bg-blue-500 text-white rounded px-2 py-1 hover:bg-blue-800 focus:outline-none cursor-pointer">
+                      <button
+                        className="bg-blue-500 text-white rounded px-2 py-1 hover:bg-blue-800 focus:outline-none cursor-pointer"
+                        onClick={() => handleValidar(item._id)}
+                      >
                         Validar
                       </button>
                     </td>
                     <td className="p-2 text-center">
-                      <button className="bg-red-500 text-white rounded px-2 py-1 hover:bg-red-800 focus:outline-none cursor-pointer">
+                      <button
+                        className="bg-red-500 text-white rounded px-2 py-1 hover:bg-red-800 focus:outline-none cursor-pointer"
+                        onClick={() => handleInvalidar(item._id)}
+                      >
                         Deshabilitar
                       </button>
                     </td>
@@ -60,9 +79,10 @@ const Page = () => {
             </p>
           )}
         </div>
+        <Toaster position="top-center" />
       </div>
     </div>
   );
 };
 
-export default Page; // Asegúrate de exportar el componente con la letra mayúscula
+export default Page;
